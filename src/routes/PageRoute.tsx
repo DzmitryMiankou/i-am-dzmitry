1be13010510f8bb987291818fd8a2b3fd6a9691b
DzmitryMiankou <gmiankou@gmail.com>
2023-07-18 22:56:18 +0300

    del st

--- src/routes/PageRoute.tsx
+++ src/routes/PageRoute.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "../components/mainPage/MainPage";
 import EducationPage from "../components/educationPage/EducationPage";
 import StackPage from "../components/stackPage/StackPage";
 import ExamplePage from "../components/examplePage/ExamplePage";
-
-const H1 = styled.h1`
-  color: #aa0000;
-`;
 
 const PageRoute = () => {
   const arrToElem: Array<{ element: JSX.Element; path: string }> = [
